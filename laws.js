07155aa4a13937dbb0b9b7fb06a7389d98bcec83
mitul-7494 <77108292+mitul-7494@users.noreply.github.com>
2024-04-23 19:30:27 +0530

    Update laws.js

--- laws.js
+++ laws.js
@@ -32,16 +32,58 @@ function getDifferenceInDays(dateString1, dateString2) {
     return differenceInDays + remdayweek;
 }
 
-var x = {'state':['new']};
+//
+function getDayOfWeek(dateString) {
+    const parts = dateString.split('-');
+    const day = parseInt(parts[0], 10);
+    const monthAbbrev = parts[1];
+    const year = parseInt(parts[2], 10);
 
-function StateOld(){
-    x['state'] = ['old'];
+    const monthAbbreviations = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const month = monthAbbreviations.findIndex(abbr => abbr === monthAbbrev);
+    const date = new Date(year, month, day);
+    return date.getDay();
 }
 
-function StateNew(){
-    x['state'] = ['new'];
+//mapping of expenditure to tr attribute
+var expenditureMap = {
+  "Annual Leave": "0",
+  "Bank Holidays": "1",
+  "Contracted Hours - Employee": "2",
+  "Employee Volunteering and Fundraising": "3",
+  "Extra Hours - Employee": "4",
+  "Leavers/Joiners": "5",
+  "Mace Day": "6",
+  "Other Leave - Christmas Close Down": "7",
+  "Other Leave - Compassionate": "8",
+  "Other Leave - Jury Service": "9",
+  "Other Leave - Medical Appointments": "10",
+  "Other Leave - Paid": "11",
+  "Other Leave - Study Leave": "12",
+  "Other Leave - Unpaid": "13",
+  "Other Leave - Voluntary Reserve Forces Leave": "14",
+  "Parental Leave": "15",
+  "Sickness (Long Term) (GIP)": "16",
+  "Sickness (Short Term)": "17",
+  "Training": "18"
 }
 
-function PrintState(){
-    console.log(JSON.stringify(x));
+
+var currentTimeCardState = {
+    "AbsentType" : {},
+    "NumberOfAT" : 0,
+    "startingDay" : 0,
+    "endingDay" : 6,
+}
+
+function SetCardState(){
+    let numberOfAT = document.getElementsByClassName('xwn').length - 5;
+    currentTimeCardState["NumberOfAT"] = numberOfAT;
+    if(numberOfAT == 0){
+        return;
+    }else{
+        for(let i=0;i<numberOfAT;i++){
+            
+        }
+    }
 }
